9b75a904ddb4447c6ded0e70fe59d2063a56e91a
Alan <Vaisentcs@gmail.com>
2018-10-18 17:37:25 -0300

    Initial commit

---
+++ app/src/main/java/formularioz/com/ar/formularioz/MainActivity_formulariologin.kt
@@ -0,0 +1,12 @@
+package formularioz.com.ar.formularioz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_formulariologin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_formulariologin)
+    }
+}
